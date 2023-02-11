5d1806bc8e5d8a7dc6d5fb7dc96013f2e5882584
nguyenclaire <nguyenclaire001@gmail.com>
2023-02-11 00:06:54 -0600

    first commit

---
+++ R/myfunc.R
@@ -0,0 +1,13 @@
+#' @title Function Formula Quadratic
+#'
+#' @param x variable as an integer
+#'
+#' @return it returns a line of the equation
+#' @export
+#'
+#' @examples
+#' \dontrun function(1)
+myfunc = function(x)
+{
+  0.86089580 + 1.46959217*x - 0.02745726*x^2
+}

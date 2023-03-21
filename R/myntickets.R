7fe95d22c452fbc022eac5b31f024b240405cd67
nguyenclaire <nguyenclaire001@gmail.com>
2023-03-21 16:02:07 -0500

    done

--- R/myntickets.R
+++ R/myntickets.R
@@ -6,7 +6,7 @@
 #'
 #' @return a continuous and discrete graph and a list containing the discrete distribution
 #' and normal approximation
-#' @importFrom stats pbinom
+#' @importFrom stats pbinom optimize
 #' @importFrom graphics abline
 #' @export
 #'
@@ -20,13 +20,19 @@ ntickets <- function(N = 200, gamma = 0.02, p = 0.95) {
   cat("LIST:\n")
   print(x)
 
-  ind <- which.min(abs(nd))
-  ind2 <- which.min(abs(nc))
+  ind <- which.min(abs(nd)) # discrete
+
+  # Define the objective function for the normal approximation
+  f <- function(n) {
+    abs(1 - gamma - pnorm(N + 0.5, n * p, sqrt(n * p * (1 - p))))
+  }
+
+  # Find the value of n that minimizes the objective function for the normal approximation
+  ind2 <- optimize(f, interval = c(N, floor(N + N/10)), tol = 0.0001)$minimum
 
   plot(n, nd, type = 'b', main=paste("Objective Vs n to find optimal tickets sold\n", "(", n[ind], ")", "gamma = ", gamma, "N = ", N, "discrete"), ylab = "Objective", pch = 16, col = "blue")
   abline(h = 0, v = n[ind], lwd = 2, col = "red")
 
-  plot(n, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", n[ind2], ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective", col = "red")
-  abline(h = 0, v = n[ind2], col = "blue", lwd = 2)
-
+  plot(n, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", round(ind2, 4), ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective", col = "red")
+  abline(h = 0, v = round(ind2, 4), col = "blue", lwd = 2)
 }

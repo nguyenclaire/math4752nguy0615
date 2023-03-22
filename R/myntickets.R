8ef38d9cc775fc39fcedd7d7f8fe63282d842609
nguyenclaire <nguyenclaire001@gmail.com>
2023-03-22 09:59:00 -0500

    added ntickets

--- R/myntickets.R
+++ R/myntickets.R
@@ -12,13 +12,10 @@
 #'
 #' @examples ntickets(400, 0.02, 0.95)
 ntickets <- function(N = 200, gamma = 0.02, p = 0.95) {
+
   n <- seq(N, floor(N + N/10), by = 1)
   nd <- 1 - gamma - pbinom(q = N, size = n, prob = p) # discrete distribution
   nc <- 1 - gamma - pnorm(N + 0.5,  n * p, sqrt(n * p * (1 - p))) # continuous distribution
-
-  x <- list(nd = nd, nc = nc, N = N, p = p, gamma = gamma)
-  cat("LIST:\n")
-  print(x)
 
   ind <- which.min(abs(nd)) # discrete
 
@@ -28,11 +25,14 @@ ntickets <- function(N = 200, gamma = 0.02, p = 0.95) {
   }
 
   # Find the value of n that minimizes the objective function for the normal approximation
-  ind2 <- optimize(f, interval = c(N, floor(N + N/10)), tol = 0.0001)$minimum
+  ind2 <- optimize(f, interval = c(N, floor(N + N/10)))$minimum
+
+  x <- list(nd = n[ind], nc = ind2, N = N, p = p, gamma = gamma)
+  print(x)
 
   plot(n, nd, type = 'b', main=paste("Objective Vs n to find optimal tickets sold\n", "(", n[ind], ")", "gamma = ", gamma, "N = ", N, "discrete"), ylab = "Objective", pch = 16, col = "blue")
   abline(h = 0, v = n[ind], lwd = 2, col = "red")
 
-  plot(n, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", round(ind2, 4), ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective", col = "red")
-  abline(h = 0, v = round(ind2, 4), col = "blue", lwd = 2)
+  plot(n, nc, type = 'l', main=paste("Objective Vs n to find optimal tickets sold\n", "(", ind2, ")", "gamma = ", gamma, "N = ", N, "continuous"), ylab = "Objective", col = "red")
+  abline(h = 0, v = ind2, col = "blue", lwd = 2)
 }

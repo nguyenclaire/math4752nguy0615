a0d3b3590ee39a37329239255b6b2d65da41c426
nguyenclaire <nguyenclaire001@gmail.com>
2023-04-05 16:16:10 -0500

    passed cmd check results

---
+++ R/ddt.R
@@ -0,0 +1,15 @@
+#' DDT data set.
+#'
+#' A data set collected by the US forces from the tennessee river and its tributaries
+#'
+#' @format A data frame with 144 rows and 5 variables:
+#' \describe{
+#'   \item{LENGTH}{length in cm}
+#'   \item{WEIGHT}{weight of the fish, in gms}
+#'   \item{DDT}{ddt amount}
+#'   \item{MILE}{length in miles}
+#'   \item{RIVER}{type of river}
+#'   \item{SPECIES}{type of species}
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+"ddt"
